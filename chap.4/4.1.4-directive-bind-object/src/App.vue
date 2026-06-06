e773e396372f1c40f50fe2645744a602de555dec
MinnieMinwoo(Minwoo Pak) <nhs075241@snu.ac.kr>
2026-06-06 21:26:25 +0900

    chap 4.1: データバインディングのディレクティブ

---
+++ chap.4/4.1.4-directive-bind-object/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const imgAttributes = ref({
+  src: '/images/logo.svg',
+  alt: 'Vueのロゴ',
+  width: 75,
+  height: 75,
+})
+</script>
+
+<template>
+  <p><img v-bind="imgAttributes" /></p>
+  <p><img v-bind="imgAttributes" title="ゴロです！" /></p>
+  <p><img v-bind="imgAttributes" alt="ゴロです！" /></p>
+</template>
---
+++ chap.4/4.1.6-directive-class/src/App.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+type ComputedStylesReturn = {
+  textColorRed: boolean
+  bgColorBlue: boolean
+}
+
+const msg = ref('こんにちは！世界')
+
+const isTextColorRed = ref(true)
+const isBgColorBlue = ref(false)
+
+const styles = ref({
+  textColorRed: false,
+  bgColorBlue: true,
+})
+
+const computedStyles = computed((): ComputedStylesReturn => {
+  const randomText = Math.round(Math.random())
+  const textColorFlag = randomText === 0
+
+  const randomBg = Math.round(Math.random())
+  const bgColorFlag = randomBg === 0
+
+  return {
+    textColorRed: textColorFlag,
+    bgColorBlue: bgColorFlag,
+  }
+})
+</script>
+
+<template>
+  <p v-bind:class="{ textColorRed: true, bgColorBlue: true }">{{ msg }}</p>
+  <p v-bind:class="{ textColorRed: isTextColorRed, bgColorBlue: isBgColorBlue }">{{ msg }}</p>
+  <p v-bind:class="{ textColorPink: true }">{{ msg }}</p>
+  <p v-bind:class="{ 'text-color-pink': true }">{{ msg }}</p>
+  <p class="textSize24" v-bind:class="{ textColorRed: isTextColorRed, bgColorBlue: isBgColorBlue }">
+    {{ msg }}
+  </p>
+  <p class="textSize24" v-bind:class="styles">{{ msg }}</p>
+  <p v-bind:class="computedStyles">{{ msg }}</p>
+</template>
+
+<style>
+.textColorRed {
+  color: red;
+}
+.text-color-pink {
+  color: pink;
+}
+.bgColorBlue {
+  background-color: blue;
+}
+.textSize24 {
+  font-size: 24px;
+}
+</style>
