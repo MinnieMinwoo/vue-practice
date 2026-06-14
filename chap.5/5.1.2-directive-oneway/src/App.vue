f804eca577849aa078e5e1411bb069cf533e0fe6
MinnieMinwoo(Minwoo Pak) <nhs075241@snu.ac.kr>
2026-06-14 21:05:10 +0900

    chap 5.1: 双方向データバインディング

---
+++ chap.5/5.1.2-directive-oneway/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputNameBind = ref('しんちゃん')
+const inputNameOn = ref('ななし')
+const onInputNmae = (event: Event) => {
+  if (!(event.target instanceof HTMLInputElement)) return
+  const element: HTMLInputElement = event.target
+  inputNameOn.value = element.value
+}
+</script>
+
+<template>
+  <section>
+    <input type="text" v-bind:value="inputNameBind" />
+  </section>
+  <br />
+  <section>
+    <input type="text" v-on:input="onInputNmae" />
+    <p>{{ inputNameOn }}</p>
+  </section>
+</template>
---
+++ chap.5/5.1.3-directive-twoway/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const inputName2Way = ref('双方向')
+const onInputName2Way = (event: Event) => {
+  if (!(event.target instanceof HTMLInputElement)) return
+  const element: HTMLInputElement = event.target
+  inputName2Way.value = element.value
+}
+</script>
+
+<template>
+  <section>
+    <input type="text" v-bind:value="inputName2Way" v-on:input="onInputName2Way" />
+    <p>{{ inputName2Way }}</p>
+  </section>
+</template>
