96226b7306796f53ef7d71952393287e1215fb1d
MinnieMinwoo(Minwoo Pak) <nhs075241@snu.ac.kr>
2026-06-21 20:58:22 +0900

    chap 6.2: ループのディレクティブ

---
+++ chap.6/6.2-directive-conditional-basic/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const cocktailListInit: string[] = ['ホワイトレディ', 'ブルーハワイ', 'ニューヨーク']
+const cocktailList = ref(cocktailListInit)
+</script>
+
+<template>
+  <ul v-for="cocktailName in cocktailList" v-bind:key="cocktailName">
+    <li>{{ cocktailName }}</li>
+  </ul>
+  <ul v-for="(cocktailName, index) in cocktailList" v-bind:key="cocktailName">
+    <li>{{ cocktailName }}(インデックス{{ index }})</li>
+  </ul>
+</template>
---
+++ chap.6/6.2.5-directive-loop-arrayobj/src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Cocktail {
+  id: number
+  name: string
+  price: number
+}
+
+const cocktailDataListInit: Cocktail[] = [
+  { id: 2345, name: 'ホワイトレディ', price: 1200 },
+  { id: 4412, name: 'ブルーハワイ', price: 1500 },
+  { id: 6792, name: 'ニューヨーク', price: 1100 },
+  { id: 8429, name: 'マティーニ', price: 1500 },
+]
+
+const cocktailDataList = ref(cocktailDataListInit)
+</script>
+
+<template>
+  <ul>
+    <li v-for="cocktail in cocktailDataList" v-bind:key="cocktail.id">
+      {{ cocktail.name }}の値段は{{ cocktail.price }}円
+    </li>
+  </ul>
+</template>
