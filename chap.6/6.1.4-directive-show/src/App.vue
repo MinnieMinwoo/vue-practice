2bc0497ead19a9a348a04ded08f534b8fc4c5096
MinnieMinwoo(Minwoo Pak) <nhs075241@snu.ac.kr>
2026-06-21 20:40:15 +0900

    chap 6.1: 条件分岐のディレクティブ

---
+++ chap.6/6.1.4-directive-show/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+const number = ref(80)
+const showOrNot = computed(() => {
+  let showOrNot = false
+
+  const rand = Math.floor(Math.random() * 100)
+  if (rand > number.value) {
+    showOrNot = true
+  }
+
+  return showOrNot
+})
+</script>
+
+<template>
+  <section>
+    v-ifを利用
+    <p v-if="showOrNot">条件に合致したので表示</p>
+  </section>
+  v-showを利用
+  <p v-show="showOrNot">条件に合致したので表示</p>
+</template>
