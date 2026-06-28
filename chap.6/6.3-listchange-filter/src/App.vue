cd06f1684d7c215820f39768367624db8a6625f3
MinnieMinwoo <nhs075241@snu.ac.kr>
2026-06-28 23:21:39 +0900

    chap 6.3: リスト操作

---
+++ chap.6/6.3-listchange-filter/src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+interface Cocktail {
+  id: number
+  name: string
+  price: number
+}
+
+const cocktailDataListInit: Cocktail[] = [
+  {
+    id: 2345,
+    name: 'ホワイトレディ',
+    price: 1200,
+  },
+  {
+    id: 4412,
+    name: 'ブルーハワイ',
+    price: 1500,
+  },
+  {
+    id: 6792,
+    name: 'ニューヨーク',
+    price: 1100,
+  },
+  {
+    id: 1234,
+    name: 'マティーニ',
+    price: 1500,
+  },
+]
+
+const cocktailDataList = ref(cocktailDataListInit)
+const cocktail1500 = computed((): Cocktail[] => {
+  const newList = cocktailDataList.value.filter((cocktailItem: Cocktail): boolean => {
+    return cocktailItem.price === 1500
+  })
+  return newList
+})
+</script>
+
+<template>
+  <section>
+    全てのカクテルリスト
+    <ul>
+      <li v-for="cocktail in cocktailDataList" v-bind:key="cocktail.id">
+        {{ cocktail.name }}の値段は{{ cocktail.price }}円
+      </li>
+    </ul>
+  </section>
+  <section>
+    値段が1500円のカクテルリスト
+    <ul>
+      <li v-for="cocktailItem in cocktail1500" v-bind:key="'cocktail1500' + cocktailItem.id">
+        {{ cocktailItem.name }}の値段は{{ cocktailItem.price }}円
+      </li>
+    </ul>
+  </section>
+</template>
---
+++ chap.6/6.3.4-listchange-obj/src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const whiteLadyInit = {
+  id: 2345,
+  name: 'ホワイトレディ',
+  price: 1200,
+  recipe: 'ジン30ml+コワントロー15ml+レモン果汁15ml',
+}
+const whiteLady = ref(whiteLadyInit)
+
+const changeWhiteLadyPrice = () => {
+  whiteLady.value.price = 1500
+}
+</script>
+
+<template>
+  <dl>
+    <template v-for="(value, key) in whiteLady" v-bind:key="key">
+      <dt>{{ key }}</dt>
+      <dd>{{ value }}</dd>
+    </template>
+  </dl>
+  <p>
+    価格を1500円に
+    <button v-on:click="changeWhiteLadyPrice">変更</button>
+  </p>
+</template>
