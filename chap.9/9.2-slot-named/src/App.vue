fc2b0a72d219660d023cf0b228568eb2d3e58414
Minnie(Minwoo Pak) <stellahoshi0507@gmail.com>
2026-07-08 17:13:55 +0900

    chap 9.2: 複数のSlotを実現する名前付きSlot

---
+++ chap.9/9.2-slot-named/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import OneSection from './components/OneSection.vue'
+
+const tartProblemsInit = ['田はが通じません。', '留守です。']
+const taro = ref('田中太郎')
+const taroProblems = ref(tartProblemsInit)
+const jiro = ref('鈴木次郎')
+</script>
+
+<template>
+  <section>
+    <h2>Slotの利用</h2>
+    <OneSection v-bind:name="taro">
+      <template v-slot:default>
+        <p>問題発生</p>
+      </template>
+      <template v-slot:detail>
+        <ul>
+          <li v-for="problem in taroProblems" v-bind:key="problem">
+            {{ problem }}
+          </li>
+        </ul>
+      </template>
+    </OneSection>
+    <OneSection v-bind:name="jiro" />
+  </section>
+</template>
+
+<style scoped></style>
