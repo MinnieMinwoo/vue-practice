740c896494c753c4ebbc3ad04da0a2946f21411b
MinnieMinwoo <stellahoshi0507@gmail.com>
2026-07-09 16:18:50 +0900

    chap 9.4: 動的コンポーネント

---
+++ chap.9/9.4-components-dynamic/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Input from './components/Input.vue';
+import Radio from './components/Radio.vue';
+import Select from './components/Select.vue';
+
+
+const currentComponent = ref(Input);
+const currentComponentName = ref('Input');
+const componentList = [Input, Radio, Select];
+const componentNameList = ['Input', 'Radio', 'Select'];
+
+let currnetComponentIndex = 0;
+const switchComponent = () => {
+  currnetComponentIndex++;
+  if (currnetComponentIndex >= componentList.length)
+    currnetComponentIndex = 0;
+  
+  currentComponent.value = componentList[currnetComponentIndex] ?? Input;
+  currentComponentName.value = componentNameList[currnetComponentIndex] ?? "Input";
+}
+
+</script>
+  
+<template>
+  <p>コンポーネント名: {{ currentComponentName }}</p>
+  <KeepAlive>
+    <component v-bind:is="currentComponent" />
+  </KeepAlive>
+  <button v-on:click="switchComponent">切り替え</button>
+</template>
+
+<style scoped></style>
