32c6bf8bd9d5c9745470a6b3f4d12e53330336b8
Minnie(Minwoo Pak) <stellahoshi0507@gmail.com>
2026-07-09 16:20:12 +0900

    chap 9.3: データを受け渡しを逆転させるスコープ付きSlot

---
+++ 9.3-slot-scopped/src/components/OneSection.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const memberInfo = reactive({
+  name: '田中太郎',
+  state: '問題ありません。',
+})
+</script>
+
+<template>
+  <section>
+    <slot v-bind:memberInfo="memberInfo">
+      <p>{{ memberInfo.name }}さんの状況</p>
+      <p>{{ memberInfo.state }}</p>
+    </slot>
+  </section>
+</template>
