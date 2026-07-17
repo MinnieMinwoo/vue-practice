d922ab48f6b1c17e36bbe28303648cc06d433dfe
MinnieMinwoo <=>
2026-07-17 18:48:29 +0900

    chap 10.3: ルートパラメータ

--- chap.10/10.2-router-basic/src/router/index.ts
+++ chap.10/10.2-router-basic/src/router/index.ts
@@ -12,6 +12,17 @@ const routeSettings: RouteRecordRaw[] = [
     name: 'MemberList',
     component: () => import('@/views/member/MemberList.vue'),
   },
+  {
+    path: '/member/detail/:id',
+    name: 'MemberDetail',
+    component: () => import('@/views/member/MemberDetail.vue'),
+    props: (routes) => {
+      const idNumber = Number(routes.params.id)
+      return {
+        id: idNumber,
+      }
+    }
+  }
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ chap.10/10.2-router-basic/src/views/member/MemberDetail.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import {computed, inject} from 'vue';
+import { RouterLink } from 'vue-router';
+import type { Member } from "../../interfaces";
+
+interface Props {
+  id: number;
+}
+
+const props = defineProps<Props>();
+const memberList = inject<Map<number, Member>>('memberList');
+const member = computed(() => memberList?.get(props.id) as Member);
+console.log('props.id', props.id);
+console.log('memberList', memberList);
+console.log('member.value', member.value);
+
+const localNote = computed(() => {
+  if (!member.value) return '--';
+  return member.value.note;
+})
+</script>
+
+<template>
+  <h1>会員管理</h1>
+  <nav id="breadcrumbs">
+    <ul>
+      <li><RouterLink v-bind:to="{name: 'AppTop'}">TOP</RouterLink></li>
+      <li>会員リスト</li>
+    </ul>
+  </nav>
+  <section>
+    <h2>会員詳細情報</h2>
+    <dl>
+      <dt>ID</dt>
+      <dd>{{ props.id }}</dd>
+      <dt>名前</dt>
+      <dd>{{ member.name }}</dd>
+      <dt>メールアドレス</dt>
+      <dd>{{ member.email }}</dd>
+      <dt>保有ポイント</dt>
+      <dt>備考</dt>
+      <dd>{{ localNote }}</dd>
+    </dl>
+  </section>
+</template>
